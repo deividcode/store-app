2a9bc98eb717f349519e26a8c1f9acbdac5112f8
Jose David <quinteromda@gmail.com>
2024-05-21 12:59:53 -0400

    Add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     <h1>Hola Mundo</h1>
-    </>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
